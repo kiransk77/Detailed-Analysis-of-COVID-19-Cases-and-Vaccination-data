bfa6a888792429386ea0797d0b883459da55f078
Sai Kiran Tanikella <92815336+kiransk77@users.noreply.github.com>
2022-08-24 10:54:08 +0530

    Add files via upload

---
+++ Covid-19/assign1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+chmod +x neighbor-districts-script.sh
+./neighbor-districts-script.sh
+chmod +x edge-generator.sh
+./edge-generator.sh
+chmod +x case-generator.sh
+./case-generator.sh
+chmod +x peaks-generator.sh
+./peaks-generator.sh
+chmod +x vaccinated-count-generator.sh
+./vaccinated-count-generator.sh
+chmod +x vaccination-population-ratio-generator.sh
+./vaccination-population-ratio-generator.sh
+chmod +x vaccine-type-ratio-generator.sh
+./vaccine-type-ratio-generator.sh
+chmod +x vaccinated-ratio-generator.sh
+./vaccinated-ratio-generator.sh
+chmod +x complete-vaccination-generator.sh
+./complete-vaccination-generator.sh
